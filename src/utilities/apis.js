f344755c411619e62950eabb27612c66967411fb
Adhy <adhywiranatap@gmail.com>
2017-09-27 11:51:09 +0700

    update react native config

--- src/utilities/apis.js
+++ src/utilities/apis.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
+import Config from 'react-native-config';
 
-const apiKey = '781c0d54f287466d8323a7955b9ce31a';
+const apiKey = Config.NEWS_API_KEY;
+// const apiKey = '781c0d54f287466d8323a7955b9ce31a';
 
 export const fetchSources = () => (
   axios.get(`https://newsapi.org/v1/sources?language=en&apiKey=${apiKey}`)
